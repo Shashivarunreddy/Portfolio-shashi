bfdb9b32a691b8c135d8d5bd6b1ab28192f064ac
harshdeep <harshdeepsinghbakshi2005@gmail.com>
2024-10-27 07:01:09 +0530

    Hero and Projects Animation

--- src/components/HeroOrbit.tsx
+++ src/components/HeroOrbit.tsx
@@ -1,15 +1,54 @@
-import { PropsWithChildren } from "react"
+import { PropsWithChildren } from "react";
+import { twMerge } from "tailwind-merge";
 
-export const HeroOrbit = ({children, size, rotation}: PropsWithChildren<{size: number, rotation:number}>) => {
-    return (
-        <div className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 ">
-        <div className="" 
-        style={{rotate:`${rotation}deg` ,height: `${size}px`, width: `${size}px`}}>
-          <div className="inline-flex" 
-          style={{transform: `rotate(${rotation*(-1)}def)`}}>
-            {children}
+export const HeroOrbit = ({
+  children,
+  size,
+  rotation,
+  className,
+  orbitDuration,
+  spinDuration,
+  shouldOrbit = false,
+  shouldSpin = false
+}: PropsWithChildren<{
+  size: number;
+  rotation: number;
+  className?: string;
+  orbitDuration?: number;
+  spinDuration?: number;
+  shouldOrbit?: boolean;
+  shouldSpin?: boolean;
+}>) => {
+  return (
+    <div
+      className={twMerge(
+        "absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 -z-20",
+        className
+      )}
+    >
+      <div
+        className={twMerge(shouldOrbit === true && "animate-spin")}
+        style={{ animationDuration: `${orbitDuration}s` }}
+      >
+        <div
+          className="flex items-start justify-start "
+          style={{
+            rotate: `${rotation}deg`,
+            height: `${size}px`,
+            width: `${size}px`,
+          }}
+        >
+          <div className={twMerge(shouldSpin===true && "animate-spin [animation-duration:10s]")}
+          style={{ animationDuration: `${spinDuration}s` }}>
+            <div
+              className="inline-flex"
+              style={{ transform: `rotate(${rotation * -1}def)` }}
+            >
+              {children}
+            </div>
           </div>
         </div>
       </div>
-    )
-}
+    </div>
+  );
+};
--- src/sections/Projects.tsx
+++ src/sections/Projects.tsx
@@ -53,10 +53,13 @@ export const ProjectsSection = () => {
         <SectionHeader title="Real-world Results" eyebrow="Featured Projects" description="See how I transformed concepts into engaging digital experiences."/>
         
         <div className="mt-10 md:mt-20 flex flex-col gap-20">
-          {portfolioProjects.map((project) => (
+          {portfolioProjects.map((project, idx) => (
             <Card
               key={project.title}
-              className="px-8 pt-8 md:pt-12 md:px-10 lg:pt-16 lg:px-20 pb-0"
+              className="px-8 pt-8 md:pt-12 md:px-10 lg:pt-16 lg:px-20 pb-0 sticky"
+              style={{
+                top: `calc(64px + ${idx * 40}px)`
+              }}
             >
               <div
                 className="absolute inset-0 -z-10 opacity-5"
