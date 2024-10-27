90bdbe2eb25f03822ea0ffab146067514c06d25b
harshdeep <harshdeepsinghbakshi2005@gmail.com>
2024-10-27 18:43:59 +0530

    Fixed Map

--- src/sections/About.tsx
+++ src/sections/About.tsx
@@ -167,7 +167,7 @@ export const AboutSection = () => {
               <Image
                 src={mapImage}
                 alt="map"
-                className="h-full w-full object-cover object-left-top"
+                className="h-full w-full object-cover sm:object-fill object-left-top"
               />
               <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 size-20 rounded-full after:content-[''] after:absolute after:inset-0 after:outline after:outline-2 after:-outline-offset-2 after:rounded-full after:outline-gray-950/30">
                 <div className="absolute inset-0 rounded-full bg-gradient-to-r from-emerald-300 to-sky-400 -z-20 animate-ping [animation-duration:2s]"></div>
